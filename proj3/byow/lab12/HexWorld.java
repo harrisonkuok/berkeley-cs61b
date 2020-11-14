17d666c9c07e167bbd14911c0346d82ce6b6d8f2
harrisonkuok <you@berkeley.edu>
2020-11-13 16:48:23 -0800

    finish lab12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,89 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    public static void addHexagon(TETile[][] tiles, int s, int startPosX, int startPosY) {
+        TETile tile = randomTile();
+        int lineStartPos = 0;
+        int lineLength = 0;
+        for (int y = 0; y < s * 2; y += 1) {
+            lineStartPos = lineStartPosFinder(startPosX, s, y);
+            lineLength = lineLengthFinder(s, y);
+            drawLine(lineLength, lineStartPos, startPosY + y, tiles, tile);
+        }
+    }
+
+    private static int lineLengthFinder(int s, int level) {
+        if (level < s) {
+            return s + 2 * level;
+        } else {
+            return s + (s - 1) * 2 - (level - s) * 2;
+        }
+    }
+
+    private static int lineStartPosFinder(int startPosX, int s, int level) {
+        if (level < s) {
+            return startPosX - level;
+        } else {
+            return startPosX - 2 * s + (level + 1);
+        }
+    }
+
+    private static void drawLine(int length, int startX, int y, TETile[][] tiles, TETile tile) {
+        for (int i = 0; i < length; i += 1, startX += 1) {
+            tiles[startX][y] = tile;
+        }
+    }
+
+    public static void drawOneSThreeHexColumn(TETile[][] tiles, int numHex, int startX) {
+        int startY = (5 - numHex) * 3;
+        for (int i = numHex; i > 0; i -= 1, startY += 6) {
+            addHexagon(tiles, 3, startX, startY);
+        }
+    }
+
+    public static void drawSThreeHexs(TETile[][] tiles) {
+        int startX =  3;
+        for (int numHex = 3; numHex < 6; numHex += 1, startX += 5) {
+            drawOneSThreeHexColumn(tiles, numHex, startX);
+        }
+        for (int numHex = 4; numHex > 2; numHex -= 1, startX += 5) {
+            drawOneSThreeHexColumn(tiles, numHex, startX);
+        }
+    }
+
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(2);
+        switch (tileNum) {
+            case 0: return Tileset.WALL;
+            case 1: return Tileset.FLOWER;
+            default: return Tileset.WALL;
+        }
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] tiles = new TETile[WIDTH][HEIGHT];
+
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        //addHexagon(tiles, 5, 20, 20);
+
+        drawSThreeHexs(tiles);
+
+        ter.renderFrame(tiles);
+    }
+
+
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -4,22 +4,22 @@
 After looking at your own implementation, consider the implementation provided near the end of the lab.
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
-Answer:
+Answer: The use of helper functions is very useful and this would simplify the implemention and making the alorithm in different modules make the program easier to modify.
 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
-Answer:
+Answer: Each hexagon in project 3 can be seen as either a room or a hallway and we are randomly generating hallways and room and join them together.
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
-Answer:
+Answer: Drawing a hallway and drawing a room.
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: A hallway would only have witdth of length that is 1 while a room has with and length that is larger than 1.
